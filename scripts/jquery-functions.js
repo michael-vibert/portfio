11802ebdab84d0ff103ae3cb56b6eb5ad579f8ba
michael-vibert <mike160384@hotmail.com>
2022-11-02 09:11:06 +0800

    Contact page layout done, added jquery for slide effect on projects

---
+++ scripts/jquery-functions.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $(".container").on('click',function(){
+    $(this).children(".overlay").toggleClass("show");
+  });
+});
+// https://stackoverflow.com/questions/45024878/creating-onclick-image-overlay
+
